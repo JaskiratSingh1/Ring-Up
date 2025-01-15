866425ae52dd764a51b29fe1e2dc772ba7e167ba
Jaskirat Singh <jaskirat1999@gmail.com>
2025-01-15 09:43:58 -0700

    Initial Commit

---
+++ Ring Up/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ring Up
+//
+//  Created by Jaskirat Singh on 2025-01-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ring Up/Ring_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ring_UpApp.swift
+//  Ring Up
+//
+//  Created by Jaskirat Singh on 2025-01-15.
+//
+
+import SwiftUI
+
+@main
+struct Ring_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
